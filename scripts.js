e5b508f4389c87a38a3820213ceb3d6826d9b600
Dustin <dustinbradley45@live.com>
2019-03-17 15:19:51 -0400

    more styles. Nav done

--- scripts.js
+++ scripts.js
@@ -6,6 +6,18 @@ const myPortfolio = {
 $(function () {
     myPortfolio.init();
 })
+
+$(".desktop").on("click", () => {
+    $("object").animate({"width": "100%"})
+})
+
+$(".tablet").on('click', () => {
+    $("object").animate({ "width": "50%" });
+})
+
+$(".mobile").on("click", () => {
+    $("object").animate({ "width": "30%" });
+})
         
 
     myPortfolio.projectInfo = [{
@@ -24,9 +36,9 @@ $(function () {
         },
         {
             projectid: 2,
-            imageDesktop: "I am an image",
-            imageIpad: "I am an ipad size Image",
-            imagePhone: "I am an iphone size image",
+            imageDesktop: "./assets/Broken Telephone Desktop.png",
+            imageIpad: "./assets/Broken Telephone Tablet.png",
+            imagePhone: "./assets/Broken Telephone- Mobile.png",
             title: "Broken Phone",
             description: "Guess who said this famous quote after it has been sent through the translator and back again. Using HTML, SASS, javaScript, jQuery and the Google Translate RESTful API.",
             languages: [
@@ -37,8 +49,8 @@ $(function () {
                 "RESTful API",
                 "Paired Programming"
             ],
-            liveLink: "Link Live",
-            gitHub: ""
+            liveLink: "https://dustinbradley45.github.io/brokenPhone/#",
+            gitHub: "https://github.com/Dustinbradley45/brokenPhone"
         },
         {
             projectid: 3,
@@ -73,149 +85,25 @@ myPortfolio.changeLine = () => {
             let y = $(this).scrollTop();
             
             if (y >= aboutPosition - 300 && y < educationPosition) {
-               
-                console.log(aboutPosition,educationPosition)
                 $('.lineTwo').addClass('pageLineActive');
             }
+
             if (y >= educationPosition  && y < portfolioPosition) {
-              console.log(educationPosition, portfolioPosition, 'top')
-               console.log(y, 'where we are on scroll')
-                
                 $(".lineTwo, .lineOne, .lineFour, .lineFive").removeClass("pageLineActive");
                  $('.lineThree').addClass('pageLineActive');
             }
 
-            if (y >= portfolioPosition && y < contactPosition) {
-                
+            if (y >= portfolioPosition && y < contactPosition) {      
                 $(".lineThree, .lineTwo, .lineOne, .lineFive").removeClass("pageLineActive");
                 $('.lineFour').addClass('pageLineActive');
             }
 
-            if (y >= contactPosition) {
-              
+            if (y >= contactPosition) {         
                $(".lineFour").removeClass("pageLineActive");
                $('.lineFive').addClass('pageLineActive');
             }
         })
 }
-
-
-    
-myPortfolio.appendProjectOne = () => {
-    $(".leftProjectContainer h2").html(`<h2>${myPortfolio.projectInfo[0].title}</h2>`);
-
-    $(".responsiveIcons .desktop").addClass("activeProject");
-
-    $(".leftProjectContainer p").html(
-        `<p>${myPortfolio.projectInfo[0].description}</p>
-            <div class="languagesInvolved">
-                <p>${myPortfolio.projectInfo[0].languages}</p>
-            </div>
-            <a href="${myPortfolio.projectInfo[0].liveLink}" target="_blank">Live</a>
-             <a href="${myPortfolio.projectInfo[0].gitHub}" target="_blank">GitHub</a>
-            `);
-}
-    // myPortfolio.getDesktop = $(".desktop").on('click', function () {
-    //     $(".responsiveIcons .desktop").addClass("activeProject");
-    //     $(".responsiveIcons .mobile, .responsiveIcons .tablet").removeClass("activeProject");
-     
-         
-        // if ($(".responsiveIcons .desktop").hasClass("activeProject") === true) {
-        //     //  console.log($(".responsiveIcons .desktop").hasClass("activeProject") === true)
-        //     $(".mainProjectSpace").html(
-        //         `<img src="${myPortfolio.projectInfo[0].imageDesktop}" alt=""/>
-        //               `)
-        
-         
-    
-        // } else if ($(".responsiveIcons .tablet").hasClass("activeProject") === true) {
-        //     $(".mainProjectSpace").html(
-        //         `<img src="${myPortfolio.projectInfo[0].imageTablet}" alt=""/>
-             
-        //         `)
-        // } else {
-        //     $(".mainProjectSpace").html(
-        //         `<img src="${myPortfolio.projectInfo[0].imageTablet}" alt=""/>
-        //         `)
-    
-        // }
-
-    // })
-        myPortfolio.appendProjectTwo = () => {
-            $(".leftProjectContainer h2").html(`<h2>${myPortfolio.projectInfo[1].title}</h2>`);
-            $(".leftProjectContainer h2:after").css({
-                'opacity': 0
-            });
-
-            $(".responsiveIcons .desktop").addClass("activeProject");
-
-
-            $(".leftProjectContainer p").html(
-                `<p>${myPortfolio.projectInfo[1].description}</p>
-            <div class="languagesInvolved">
-                <p>${myPortfolio.projectInfo[1].languages}</p>
-            </div>
-            `);
-    
-            if ($(".responsiveIcons .desktop").hasClass("activeProject") === true) {
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageDesktop}" alt=""/>
-                `)
-
-            } else if ($(".responsiveIcons .tablet").hasClass("activeProject") === true) {
-                console.log($(".responsiveIcons .desktop").hasClass("activeProject") === true)
-
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageTablet}" alt=""/>
-                `)
-            } else {
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageMobile}" alt=""/>
-                `)
-            }
-
-
-        }
-
-        myPortfolio.appendProjectThree = () => {
-            $(".leftProjectContainer h2").html(`<h2>${myPortfolio.projectInfo[2].title}</h2>`);
-            $(".leftProjectContainer h2:after").css({
-                'opacity': 0
-            });
-
-            $(".responsiveIcons .desktop").addClass("activeProject");
-    
-            $(".leftProjectContainer p").html(
-                `<p>${myPortfolio.projectInfo[2].description}</p>
-            <div class="languagesInvolved">
-                <p>${myPortfolio.projectInfo[2].languages}</p>
-            </div>
-            `);
-            $(".mainProjectSpace").html(
-                `<a href="${myPortfolio.projectInfo[2].liveLink}" target="_blank">Live</a>
-             <a href="${myPortfolio.projectInfo[2].gitHub}" target="_blank">GitHub</a>`);
-    
-            if ($(".responsiveIcons .desktop").hasClass("activeProject") === true) {
-                console.log($(".responsiveIcons .desktop").hasClass("activeProject") === true)
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageDesktop}" alt=""/>
-                `)
-
-            } else if ($(".responsiveIcons .tablet").hasClass("activeProject") === true) {
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageTablet}" alt=""/>
-                `)
-            } else {
-                $(".mainProjectSpace").html(
-                    `<img src="${myPortfolio.projectInfo[1].imageMobile}" alt=""/>
-                `)
-            }
-        }
-
-
-
-
-
 
         //   FUNCTION INITIAL
 myPortfolio.init = () => {
@@ -225,26 +113,11 @@ myPortfolio.init = () => {
     // CHANGE LINES ON SIDE
     myPortfolio.changeLine()
 
-    // DEFAULT SHOW PROJECT ONE
-    myPortfolio.appendProjectOne();
-    $(".mainProjectSpace").html(
-        `<img src="${myPortfolio.projectInfo[0].imageDesktop}" alt=""/>
-                `)
-    $(".responsiveIcons .desktop").addClass("activeProject")
-
-    // CHECK TO SEE WHICH HAS A CLASS
-     
-    // myPortfolio.allProjects = () => {
-    //     myPortfolio.appendProjectOne();
-    //     myPortfolio.appendProjectTwo();
-    //     myPortfolio.appendProjectThree();
-    // }
-
-
     // SCROLL FEATURE
     $('.scrollDown').on("click", function () {
+        const y = $(window).height();
         window.scrollBy({
-            top: 821,
+            top: y,
             behavior: 'smooth'
         });
 
@@ -253,68 +126,71 @@ myPortfolio.init = () => {
         });
     });
 
+    // let controller = new ScrollMagic.Controller({
+    //     container: '.scrollContainer',
+    //     globalSceneOptions: {
+    //         triggerHook: 'onEnter'
+    //     },
+    //     vertical: false,
+    // });
 
-    // $(".responsive").on("click", () => {
-    //     if ($(this).hasClass('activeProject') === true ) {
-    //         console.log(true);
-    //         $(".responsiveIcons .tablet").addClass("activeProject");
-    //         $(".responsiveIcons .mobile, .responsiveIcons .desktop").removeClass("activeProject");
-    //         console.log("this is tablet")
-    
-    //         $(".mainProjectSpace").html(
-    //             `<img src="${myPortfolio.projectInfo[0].imageTablet}" alt=""/>
-    //                   `)
-    //     }
+    // new ScrollMagic.Scene({
+    //     triggerElement: "#sliderTitle",
+    //     duration: 100,
+    //     offset: 50
+    // }).addTo(controller);
 
-    // })
-
-
-    //  CHANGE PROJECT
-    $(".projectChange").on("click", function () {
-        if ($(this).data('id') === 1) {
-            myPortfolio.appendProjectOne();
-
-            $(".responsiveIcons button").on("click", () => {
-                if ($(this).data('id') === 1) {
-                
-                    $(".responsiveIcons .desktop").addClass("activeProject");
-                    $(".responsiveIcons .mobile, .responsiveIcons .tablet").removeClass("activeProject");
-                    console.log("this is desktop")
-                    $(".mainProjectSpace").html(
-                        `<img src="${myPortfolio.projectInfo[0].imageDesktop}" alt=""/>
-                        `)
+    myPortfolio.openNav = () => {
+        $(".headerNav").animate({
+            "width": "250px",
+            "opacity": "1"
+        })
+        $(".mainNav").animate({
+            "opacity": "0"
+        })
+        $("body").animate({
+            "marginRight": "250px"
+        })
         
-                } else if ($(this).data("id") === 2) {
-                    $(".responsiveIcons .tablet").addClass("activeProject");
-                    $(".responsiveIcons .mobile, .responsiveIcons .desktop").removeClass("activeProject");
-                    console.log("this is tablet")
+    }
 
-                    $(".mainProjectSpace").html(
-                        `<img src="${myPortfolio.projectInfo[0].imageTablet}" alt=""/>
-                    `)
+    myPortfolio.closeNav = () => {
+        $(".headerNav").animate({
+            "width": "0",
+            "opacity": "0",
+        })
+        $(".mainNav").animate({
+            "opacity": "1"
+        })
+        $("body").animate({
+            "marginRight": "0"
+        })
+    }
 
-                } else {
-                    console.log("this is mobile")
-                    $(".responsiveIcons .mobile").addClass("activeProject");
-                    $(".responsiveIcons .desktop, .responsiveIcons .tablet").removeClass("activeProject");
+    $(".mainNav").on("click", () => {
+        myPortfolio.openNav();
+        // myPortfolio.closeNav();
+    });
 
-                    $(".mainProjectSpace").html(
-                        `<img src="${myPortfolio.projectInfo[0].imageMobile}" alt=""/>
-                    `)
-               
-                }
-            })
-        } else if ($(this).data('id') === 2) {
-            myPortfolio.appendProjectTwo();
-        } else if ($(this).data('id') === 3) {
-            myPortfolio.appendProjectThree();
-        }
+    $(".close").on("click", () => {
+        myPortfolio.closeNav();
     })
+    
 
+   
+    
+    //  CHANGE PROJECT
+    // $(".projectChange").on("click", function () {
+    //     if ($(this).data('id') === 1) {
+    //         myPortfolio.appendProjectOne();
+    //     } else if ($(this).data('id') === 2) {
+    //         myPortfolio.appendProjectTwo();
+    //     } else if ($(this).data('id') === 3) {
+    //         myPortfolio.appendProjectThree();
+    //     }
+    // })
+        
+
+        
 }
-
-
-
-     
-
 
